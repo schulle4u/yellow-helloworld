ab0f2bae450e9098d99dd9172105362ad670a1fa
Steffen Schultz <steffenschultz@mailbox.org>
2018-07-21 14:23:25 +0200

    Better comments

--- release.sh
+++ release.sh
@@ -2,7 +2,10 @@
 # Prepare plugin release
 
 # Directories
+# YELLOWDIR is the absolute path to your Yellow installation
 YELLOWDIR="/home/steffen/yellow/"
+
+# PLUGINROOT is the root directory for your development projects, e.g. your GitHub folder
 PLUGINROOT="/mnt/e/Steffen/Documents/GitHub/"
 
 # Check parameters
