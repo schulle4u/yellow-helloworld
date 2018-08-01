4e27f00a153e6897ba453dabea0728bb5a728636
markseu <mark2011@mayberg.se>
2018-08-01 20:18:01 +0200

    Updated example files

--- example.php
+++ example.php
@@ -1,18 +1,17 @@
 <?php
-// Example plugin 0.7.4
-// Copyright (c) 2018 Steffen Schultz
+// Example plugin, https://github.com/schulle4u/yellow-plugin-example
+// Copyright (c) 2018 Example Name
 // This file may be used and distributed under the terms of the public license.
 
 class YellowExample
 {
-	const VERSION = "0.7.4";
+	const VERSION = "0.7.5";
 	var $yellow;			//access to API
 	
 	// Handle initialisation
 	function onLoad($yellow)
 	{
 		$this->yellow = $yellow;
-		$this->yellow->config->setDefault("exampleStyle", "MyPlugin");
 	}
 	
 	// Handle page content parsing of custom block
@@ -21,10 +20,9 @@ class YellowExample
 		$output = null;
 		if($name=="example" && $shortcut)
 		{
-			list($style) = $this->yellow->toolbox->getTextArgs($text);
-			if(empty($style)) $style = $this->yellow->config->get("exampleStyle");
-			$output = "<div class=\"".htmlspecialchars($style)."\">";
-			$output .= "<p>It works!</p>\n";
+			$output = "<div class=\"".htmlspecialchars($name)."\">";
+			$output .= "Add more HTML code here";
+			$output .= "</div>";
 		}
 		return $output;
 	}
