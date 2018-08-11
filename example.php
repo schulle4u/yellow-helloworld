f2332d7a1dd4aae116d998465bb051dc9cbd5e3d
Steffen Schultz <steffenschultz@mailbox.org>
2018-08-11 18:41:00 +0200

    New coding standard

--- example.php
+++ example.php
@@ -3,47 +3,37 @@
 // Copyright (c) 2018 Example Name
 // This file may be used and distributed under the terms of the public license.
 
-class YellowExample
-{
-	const VERSION = "0.7.6";
-	var $yellow;			//access to API
-	
-	// Handle initialisation
-	function onLoad($yellow)
-	{
-		$this->yellow = $yellow;
-	}
-	
-	// Handle page content parsing of custom block
-	function onParseContentBlock($page, $name, $text, $shortcut)
-	{
-		$output = null;
-		if($name=="example" && $shortcut)
-		{
-			$output = "<div class=\"".htmlspecialchars($name)."\">";
-			$output .= "Add more HTML code here";
-			$output .= "</div>";
-		}
-		return $output;
-	}
-	
-	/* Uncomment if needed
-	// Handle page extra HTML data
-	function onExtra($name)
-	{
-		$output = NULL;
-		if($name == "header")
-		{
-			$output .= "<!-- Additional CSS and JS for your plugin -->\n";
-		}
-		if($name == "footer")
-		{
-			$output = "<!-- It is better to include large JS files after the content -->\n";
-		}
-		return $output;
-	}
-	*/
+class YellowExample {
+    const VERSION = "0.7.6";
+    public $yellow;         //access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+    }
+    
+    // Handle page content parsing of custom block
+    public function onParseContentBlock($page, $name, $text, $shortcut) {
+        $output = null;
+        if ($name=="example" && $shortcut) {
+            $output = "<div class=\"".htmlspecialchars($name)."\">";
+            $output .= "Add more HTML code here";
+            $output .= "</div>";
+        }
+        return $output;
+    }
+    
+    // Handle page extra HTML data
+    public function onExtra($name) {
+        $output = null;
+        if ($name == "header") {
+            $output = "<!-- Include additional CSS and JavaScript here -->\n";
+        }
+        if ($name == "footer") {
+            $output = "<!-- Include large files here, after the content -->\n";
+        }
+        return $output;
+    }
 }
 
 $yellow->plugins->register("example", "YellowExample", YellowExample::VERSION);
-?>
